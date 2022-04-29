68e1112e686e2fb2714bce470791b0a73976e0c4
pankaj <pankajroth@gmail.com>
2022-04-29 18:06:00 +0530

    Initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: ./build/install/untitled6/bin/untitled6
---
+++ src/jvmMain/kotlin/me.panka.application/Server.kt
@@ -0,0 +1,41 @@
+package me.panka.application
+
+import io.ktor.application.call
+import io.ktor.html.respondHtml
+import io.ktor.http.HttpStatusCode
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+import io.ktor.http.content.resources
+import io.ktor.http.content.static
+import kotlinx.html.*
+
+fun HTML.index() {
+    head {
+        title("Hello from Ktor!")
+    }
+    body {
+        div {
+            +"Hello from Ktor"
+        }
+        div {
+            id = "root"
+        }
+        script(src = "/static/untitled6.js") {}
+    }
+}
+
+fun main() {
+    val port = System.getenv("PORT")?.toInt() ?: 9090
+    embeddedServer(Netty, port = port, host = "127.0.0.1") {
+        routing {
+            get("/") {
+                call.respondHtml(HttpStatusCode.OK, HTML::index)
+            }
+            static("/static") {
+                resources()
+            }
+        }
+    }.start(wait = true)
+}
